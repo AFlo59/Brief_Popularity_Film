56169428057c7bbb26c341ab70537a516b2a6c17
AFlo59 <etudessup59230@gmail.com>
2024-03-28 14:24:03 +0100

    up

--- Django/tailwind.config.js
+++ Django/tailwind.config.js
@@ -1,30 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    '/templates/**/*.html',
-    '/../templates/**/*.html',
-    '/**/templates/**/*.html',
-  ],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        midnight: '#0D2951',
-        light_blue: '#52C5F2',
-        gray: '#2b2d42',
-        red: '#F25041',
-        ghost_white: '#F8F8FF',
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [],
 }
 
