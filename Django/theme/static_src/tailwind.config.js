f87a8c0d9ba111d93f1effe0cb7208503ef297dc
AFlo59 <etudessup59230@gmail.com>
2024-04-02 10:29:30 +0200

    BF-17 up is work can see the light

--- Django/theme/static_src/tailwind.config.js
+++ Django/theme/static_src/tailwind.config.js
@@ -42,7 +42,15 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {},
+        extend: {
+            color: {
+                midnight: '#0D2951',
+                light_blue: '#52C5F2',
+                gray: '#2b2d42',
+                red: '#F25041',
+                ghost_white: '#F8F8FF',
+            },
+        },
     },
     plugins: [
         /**
