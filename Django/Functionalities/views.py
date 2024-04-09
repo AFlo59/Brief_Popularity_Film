d50f77d62cac5db216d9fa3747d963cabea4c090
AFlo59 <etudessup59230@gmail.com>
2024-04-09 14:30:24 +0200

    BF-65 up

--- Django/Functionalities/views.py
+++ Django/Functionalities/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import FilmScrap
+from dateutil.relativedelta import relativedelta as rd
 
 @login_required
 def recettes_page(request):
@@ -19,6 +20,11 @@ def historique_page(request):
 
 @login_required
 def nouveautes_page(request):
-    films = FilmScrap.objects.order_by("id")[:10]
-    print(films)
+    films = FilmScrap.objects.order_by("classement")[:10]
+    fmt = '{0.hours}h {0.minutes}'
+    for film in films:
+        if film.duration is not None:
+            film.duration = fmt.format(rd(seconds=film.duration))
+        if isinstance(film.genre, str):
+            film.genre = [genre.strip() for genre in film.genre.strip('[]').split(',')]
     return render(request, "functionalities/nouveautes_page.html", {"films": films})
