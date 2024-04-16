a27b9cd51f0498b16f52cdd9e30a2c5226a05e14
Elie LECAS <elielecas00@gmail.com>
2024-04-16 15:06:38 +0200

    BF-71 pipelines quai fini

--- modelisation/functions.py
+++ modelisation/functions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def classify_entrees_year(df,column):
     # print(type(valeur))
     for index,valeur in df[column].items():
@@ -70,4 +72,196 @@ def classify_entrees_year(df,column):
         else:
             df.at[index, 'entree_annee'] = 150
 
+    return df
+
+
+def calculate_director_scores(df):
+    # 1. Fréquence de réalisation
+    frequency = df['director'].value_counts()
+    # 2. Moyenne des spectateurs totaux
+    avg_total_spectators = df.groupby('director')['total_spectator'].mean()
+    # 3. Moyenne de la première semaine
+    avg_first_week = df.groupby('director')['first_week'].mean()
+    # 4. Moyenne inversée du classement hebdomadaire
+    avg_inv_hebdo_rank = 1 / df.groupby('director')['hebdo_rank'].mean()
+    # 5. Moyenne des évaluations de la presse
+    avg_rating_press = df.groupby('director')['rating_press'].mean()
+    # 5. Moyenne des récompenses
+    avg_award = df.groupby('director')['award'].mean()
+    # 6. Rendement (total_spectator / budget)
+    performance = (df.groupby('director')['total_spectator'].sum() / df.groupby('director')['budget'].sum())
+
+    # Compilation des scores dans un DataFrame
+    director_scores = pd.DataFrame({
+        'frequency': frequency,
+        'avg_total_spectators': avg_total_spectators,
+        'avg_first_week': avg_first_week,
+        'avg_inv_hebdo_rank': avg_inv_hebdo_rank,
+        'avg_rating_press': avg_rating_press,
+        'avg_award': avg_award,
+        'performance': performance
+    })
+
+    # Normalisation des scores
+    max_values = director_scores.max()
+    normalized_scores = director_scores / max_values
+
+    # Combinaison des scores normalisés
+    normalized_scores['director_combined_score'] = normalized_scores.mean(axis=1)
+    df = df.join(normalized_scores['director_combined_score'], on='director')
+   
+    return df 
+
+
+def calculate_distributor_scores(df):
+    # 1. Fréquence de distribution
+    frequency = df['distributor'].value_counts()
+    # 2. Moyenne des spectateurs totaux
+    avg_total_spectators = df.groupby('distributor')['total_spectator'].mean()
+    # 3. Moyenne de la première semaine
+    avg_first_week = df.groupby('distributor')['first_week'].mean()
+    # 4. Moyenne inversée du classement hebdomadaire
+    avg_inv_hebdo_rank = 1 / df.groupby('distributor')['hebdo_rank'].mean()
+    # 5. Moyenne des évaluations de la presse
+    avg_rating_press = df.groupby('distributor')['rating_press'].mean()
+    # 5. Moyenne des récompenses
+    avg_award = df.groupby('distributor')['award'].mean()
+    # 6. Rendement (total_spectator / budget)
+    performance = (df.groupby('distributor')['total_spectator'].sum() / df.groupby('distributor')['budget'].sum())
+
+    # Compilation des scores dans un DataFrame
+    distributor_scores = pd.DataFrame({
+        'frequency': frequency,
+        'avg_total_spectators': avg_total_spectators,
+        'avg_first_week': avg_first_week,
+        'avg_inv_hebdo_rank': avg_inv_hebdo_rank,
+        'avg_rating_press': avg_rating_press,
+        'avg_award': avg_award,
+        'performance': performance
+    })
+    # Normalisation des scores
+    max_values = distributor_scores.max()
+    normalized_scores = distributor_scores / max_values
+
+    # Combinaison des scores normalisés
+    normalized_scores['distributor_combined_score'] = normalized_scores.mean(axis=1)
+    df = df.join(normalized_scores['distributor_combined_score'], on='distributor')
+    return df
+
+
+def calculate_year_scores(df):
+    # 1. Fréquence de distribution
+    frequency = df['year'].value_counts()
+    # 2. Moyenne des spectateurs totaux
+    avg_total_spectators = df.groupby('year')['total_spectator'].mean()
+    # 3. Moyenne de la première semaine
+    avg_first_week = df.groupby('year')['first_week'].mean()
+    # 4. Moyenne du nombre de copies
+    avg_copies = df.groupby('year')['copies'].mean()
+
+    # Compilation des scores dans un DataFrame
+    year_scores = pd.DataFrame({
+        'frequency': frequency,
+        'avg_total_spectators': avg_total_spectators,
+        'avg_first_week': avg_first_week,
+        'avg_copies': avg_copies
+    })
+
+    # Normalisation des scores
+    max_values = year_scores.max()
+    normalized_scores = year_scores / max_values
+
+    # Combinaison des scores normalisés
+    normalized_scores['year_combined_score'] = normalized_scores.mean(axis=1)
+    df = df.join(normalized_scores['year_combined_score'], on='year')
+    return df
+
+def calculate_country_scores(df):
+    # 1. Fréquence de distribution
+    frequency = df['country'].value_counts()
+    # 2. Moyenne des spectateurs totaux
+    avg_total_spectators = df.groupby('country')['total_spectator'].mean()
+    # 3. Moyenne de la première semaine
+    avg_first_week = df.groupby('country')['first_week'].mean()
+    # 4. Moyenne du nombre de copies
+    avg_copies = df.groupby('country')['copies'].mean()
+
+    # Compilation des scores dans un DataFrame
+    distributor_scores = pd.DataFrame({
+        'frequency': frequency,
+        'avg_total_spectators': avg_total_spectators,
+        'avg_first_week': avg_first_week,
+        'avg_copies': avg_copies
+    })
+
+    # Normalisation des scores
+    max_values = distributor_scores.max()
+    normalized_scores = distributor_scores / max_values
+
+    # Combinaison des scores normalisés
+    normalized_scores['country_combined_score'] = normalized_scores.mean(axis=1)
+    df = df.join(normalized_scores['country_combined_score'], on='country')
+    return df
+
+
+def split_date(df):
+    df['date'] = pd.to_datetime(df['date'])
+
+    df['day'] = df['date'].dt.day
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+
+    df.drop("date", axis=1, inplace=True)
+
+    return df
+
+def classify_season(df,column):
+    for index, valeur in df[column].items():
+        if valeur == 12 or valeur == 1 or valeur == 2:
+            df.at[index, 'season'] = 'winter'
+        elif valeur == 3 or valeur == 4 or valeur == 5:
+            df.at[index, 'season'] = 'spring'
+        elif valeur == 6 or valeur == 7 or valeur == 8:
+            df.at[index, 'season'] = 'summer'
+        elif valeur == 9 or valeur == 10 or valeur == 11:
+            df.at[index, 'season'] = 'autumn'
+    
+    return df
+
+def classify_month_name(df,column):
+    for index, valeur in df[column].items():
+        if valeur == 12:
+            df.at[index, 'month_name'] = 'december'
+        elif valeur == 11:
+            df.at[index, 'month_name'] = 'novembre'
+        elif valeur == 10:
+            df.at[index, 'month_name'] = 'october'
+        elif valeur == 9:
+            df.at[index, 'month_name'] = 'september'
+        elif valeur == 8:
+            df.at[index, 'month_name'] = 'august'
+        elif valeur == 7:
+            df.at[index, 'month_name'] = 'july'
+        elif valeur == 6:
+            df.at[index, 'month_name'] = 'june'
+        elif valeur == 5:
+            df.at[index, 'month_name'] = 'may'
+        elif valeur == 4:
+            df.at[index, 'month_name'] = 'april'
+        elif valeur == 3:
+            df.at[index, 'month_name'] = 'march'
+        elif valeur == 2:
+            df.at[index, 'month_name'] = 'february'
+        elif valeur == 1:
+            df.at[index, 'month_name'] = 'january'
+    return df
+
+from vacances_scolaires_france import SchoolHolidayDates
+import datetime
+
+def is_holiday(df):
+    holiday_dates = SchoolHolidayDates()
+
+    df['is_holiday'] = df.apply(lambda row: holiday_dates.is_holiday_for_zone(datetime.date(row['year'], row['month'], row['day']), 'B'), axis=1)
+    df['is_holiday'] = df['is_holiday'].astype(int)
     return df
