752c2ac8395ff03a79bf49c67f38b455ffda5534
Elie LECAS <elielecas00@gmail.com>
2024-04-16 12:51:54 +0200

    BF-71 entree annees dans pipelines

---
+++ modelisation/functions.py
@@ -0,0 +1,73 @@
+def classify_entrees_year(df,column):
+    # print(type(valeur))
+    for index,valeur in df[column].items():
+        if valeur == 1986:
+            df.at[index, 'entree_annee'] = 168.1
+        elif valeur == 1992:
+            df.at[index, 'entree_annee'] = 116.0
+        elif valeur == 1993:
+            df.at[index, 'entree_annee'] = 132.7
+        elif valeur == 1994:
+            df.at[index, 'entree_annee'] = 124.4
+        elif valeur == 1995:
+            df.at[index, 'entree_annee'] = 130.2
+        elif valeur == 1996:
+            df.at[index, 'entree_annee'] = 136.7
+        elif valeur == 1997:
+            df.at[index, 'entree_annee'] = 149.3
+        elif valeur == 1998:
+            df.at[index, 'entree_annee'] = 170.6
+        elif valeur == 1999:
+            df.at[index, 'entree_annee'] = 153.6
+        elif valeur == 2000:
+            df.at[index, 'entree_annee'] = 165.8
+        elif valeur == 2001:
+            df.at[index, 'entree_annee'] = 187.5
+        elif valeur == 2002:
+            df.at[index, 'entree_annee'] = 184.4
+        elif valeur == 2003:
+            df.at[index, 'entree_annee'] = 173.5
+        elif valeur == 2004:
+            df.at[index, 'entree_annee'] = 195.8
+        elif valeur == 2005:
+            df.at[index, 'entree_annee'] = 175.6
+        elif valeur == 2006:
+            df.at[index, 'entree_annee'] = 188.8
+        elif valeur == 2007:
+            df.at[index, 'entree_annee'] = 178.5
+        elif valeur == 2008:
+            df.at[index, 'entree_annee'] = 190.3
+        elif valeur == 2009:
+            df.at[index, 'entree_annee'] = 201.6
+        elif valeur == 2010:
+            df.at[index, 'entree_annee'] = 207.1
+        elif valeur == 2011:
+            df.at[index, 'entree_annee'] = 217.2
+        elif valeur == 2012:
+            df.at[index, 'entree_annee'] = 203.6
+        elif valeur == 2013:
+            df.at[index, 'entree_annee'] = 193.7
+        elif valeur == 2014:
+            df.at[index, 'entree_annee'] = 209.1
+        elif valeur == 2015:
+            df.at[index, 'entree_annee'] = 205.4
+        elif valeur == 2016:
+            df.at[index, 'entree_annee'] = 213.2
+        elif valeur == 2017:
+            df.at[index, 'entree_annee'] = 209.4
+        elif valeur == 2018:
+            df.at[index, 'entree_annee'] = 201.2
+        elif valeur == 2019:
+            df.at[index, 'entree_annee'] = 213.2
+        elif valeur == 2020:
+            df.at[index, 'entree_annee'] = 65.3
+        elif valeur == 2021:
+            df.at[index, 'entree_annee'] = 95.5
+        elif valeur == 2022:
+            df.at[index, 'entree_annee'] = 152.0
+        elif valeur == 2023 or 2024:
+            df.at[index, 'entree_annee'] = 180.8
+        else:
+            df.at[index, 'entree_annee'] = 150
+
+    return df
