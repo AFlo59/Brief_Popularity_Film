ad7e3100350f20bfb7d7bd3b1a1aa3d442261a86
AFlo59 <etudessup59230@gmail.com>
2024-04-17 11:41:09 +0200

    BF-72 up

--- Django/Functionalities/views.py
+++ Django/Functionalities/views.py
@@ -6,28 +6,36 @@ from dateutil.relativedelta import relativedelta as rd
 from datetime import datetime, timedelta
 from django.db.models.functions import Upper
 
+
+
 def capitalize_name(name):
     if name:
         return ' '.join([part.capitalize() for part in name.split(' ')])
     return None
 
+
 @login_required
 def recettes_page(request):
     today = datetime.now().date()
     one_week_later = today + timedelta(days=7)
-    films = FilmScrap.objects.filter(date__range=(today, one_week_later))
-    
+    films = FilmScrap.objects.filter(date__range=(today, one_week_later)).order_by("-score_pred")
+
     for film in films:
         if film.score_pred is not None:
-            film.pred_spectateur = round((film.score_pred / 2000) / 7)
+            film.pred_spect_daily_salle1 = min(film.score_pred / 2000 / 7, 120) if film.score_pred else None
+            film.pred_spect_daily_salle2 = min(film.score_pred / 2000 / 7, 80) if film.score_pred else None
         else:
-            film.pred_spectateur = None
-            
+            film.pred_spect_daily_salle1 = None
+            film.pred_spect_daily_salle2 = None
 
     jours_semaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-    
-    return render(request, "functionalities/recettes_page.html", {"films": films, "jours_semaine": jours_semaine})
 
+    return render(request, "functionalities/recettes_page.html", {"jours_semaine": jours_semaine, "films": films})
+
+
+from datetime import datetime, timedelta
+from django.shortcuts import render
+from .models import FilmScrap
 
 @login_required
 def predict_page(request):
@@ -35,7 +43,17 @@ def predict_page(request):
     one_week_later = today + timedelta(days=7)
     films = FilmScrap.objects.filter(date__range=(today, one_week_later)).order_by("-score_pred")
 
+
+    ranking = 1
+
     for film in films:
+
+        if film.score_pred is not None:
+            film.classement = ranking
+            ranking += 1
+        else:
+            film.classement = None
+
         if film.score_pred is not None:
             film.score_pred_divided = round(film.score_pred / 2000, 2)
         else:
@@ -52,9 +70,9 @@ def predict_page(request):
             film.recette_journaliere = None
 
         if film.recette_journaliere is not None:
-             film.recette_hebdomadaire = round(film.recette_journaliere * 7, 2)
+            film.recette_hebdomadaire = round(film.recette_journaliere * 7, 2)
         else:
-             film.recette_hebdomadaire = None
+            film.recette_hebdomadaire = None
         
         if film.recette_hebdomadaire is not None:
             film.benefice_hebdomadaire = round(film.recette_hebdomadaire - 4900, 2)
@@ -76,7 +94,15 @@ def nouveautes_page(request):
     films = FilmScrap.objects.filter(date__range=(today, one_week_later)).order_by("classement")[:10]
     fmt = '{0.hours}h {0.minutes}'
 
+    ranking = 1
+
     for film in films:
+        if film.score_pred is not None:
+            film.classement = ranking
+            ranking += 1
+        else:
+            film.classement = None
+            
         if film.duration is not None:
             film.duration = fmt.format(rd(seconds=film.duration))
         if isinstance(film.genre, list):
