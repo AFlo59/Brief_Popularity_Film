4f846fa5241eb7b5e516fce8007bf8c3dd22c6bc
Maxime KUIL <maximekuil@gmail.com>
2024-04-18 12:11:38 +0200

    pipe ok

--- modelisation/converter.py
+++ modelisation/converter.py
@@ -42,6 +42,7 @@ def convert_director(df, column):
         "convert_director",
     )
     scores = load_file("director_scores")
+
     val = []
     for index, row in df.iterrows():
         try:
@@ -64,7 +65,7 @@ def convert_actor(df, column):
     for index, row in df.iterrows():
         sum = 0
         try:
-            df_actors = json.loads(df.iloc[index][column])
+            df_actors = df.iloc[index][column]
             for actor in df_actors:
                 found = scores.loc[scores["actor"] == actor]
                 if found.shape[0] != 0:
@@ -88,7 +89,7 @@ def convert_distributor(df, column):
     for index, row in df.iterrows():
         sum = 0
         try:
-            df_distributors = json.loads(df.iloc[index][column])
+            df_distributors = df.iloc[index][column]
             for distributor in df_distributors:
                 found = scores.loc[scores["distributor"] == distributor]
                 if found.shape[0] != 0:
