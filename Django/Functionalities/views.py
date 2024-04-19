cc1739f7d9983cf12f2b7715aa93563f0c69d863
maxwarch <maximekuil@gmail.com>
2024-04-19 09:12:24 +0200

    fix ajax

--- Django/Functionalities/views.py
+++ Django/Functionalities/views.py
@@ -7,10 +7,17 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from django.http import JsonResponse
 
+# Exemples de limites de capacité pour chaque salle (en commentaires)
+# Salle1: 120
+# Salle2: 80
+SALLE_CAPACITE = {"Salle1": 120, "Salle2": 80}
+
+
 def capitalize_name(name):
     if name:
-        return ' '.join([part.capitalize() for part in name.split(' ')])
+        return " ".join([part.capitalize() for part in name.split(" ")])
     return None
+
 
 def recettes_page(request):
     today = datetime.now().date()
@@ -20,79 +27,83 @@ def recettes_page(request):
     wednesday_this_week = today + timedelta(days=days_until_wednesday)
     next_tuesday = wednesday_this_week + timedelta(days=6)
 
-    films = FilmScrap.objects.filter(date__gte=wednesday_this_week, date__lte=next_tuesday).order_by("-score_pred").values('title', 'id', 'score_pred')
-
-    # Exemples de limites de capacité pour chaque salle (en commentaires)
-    # Salle1: 120
-    # Salle2: 80
-
-    salle_capacite = {"Salle1": 120, "Salle2": 80}
+    films = (
+        FilmScrap.objects.filter(date__gte=wednesday_this_week, date__lte=next_tuesday)
+        .order_by("-score_pred")
+        .values("title", "id", "score_pred")
+    )
 
     for film in films:
-        if film['score_pred'] is not None:
-            film['pred_spect_daily'] = (film['score_pred'] / 2000) / 7
-            for salle, capacite in salle_capacite.items():
+        if film["score_pred"] is not None:
+            film["pred_spect_daily"] = (film["score_pred"] / 2000) / 7
+            for salle, capacite in SALLE_CAPACITE.items():
                 # Calcul de pred_rct_daily avec une limite de capacité
                 # La limite pour Salle1 est 120, donc si pred_spect_daily * 10 dépasse 120, nous utilisons 120
                 # La limite pour Salle2 est 80, donc si pred_spect_daily * 10 dépasse 80, nous utilisons 80
-                film['pred_rct_daily_' + salle] = min(film['pred_spect_daily'] * 10, capacite)  
-                film['pred_rct_weekly_' + salle] = film['pred_rct_daily_' + salle] * 7
-                film['pred_bnf_hebdo_' + salle] = (-4900 + film['pred_rct_weekly_' + salle])
+                film["pred_rct_daily_" + salle] = min(
+                    film["pred_spect_daily"] * 10, capacite
+                )
+                film["pred_rct_weekly_" + salle] = film["pred_rct_daily_" + salle] * 7
+                film["pred_bnf_hebdo_" + salle] = (
+                    -4900 + film["pred_rct_weekly_" + salle]
+                )
         else:
-            film['pred_spect_daily'] = None
-            for salle in salle_capacite.keys():
-                film['pred_rct_daily_' + salle] = None
-                film['pred_rct_weekly_' + salle] = None
-                film['pred_bnf_hebdo_' + salle] = None
+            film["pred_spect_daily"] = None
+            for salle in SALLE_CAPACITE.keys():
+                film["pred_rct_daily_" + salle] = None
+                film["pred_rct_weekly_" + salle] = None
+                film["pred_bnf_hebdo_" + salle] = None
 
     context = {
-        'films': films,
-        'salle_capacite': salle_capacite,
+        "films": films,
+        "salle_capacite": SALLE_CAPACITE,
     }
 
     return render(request, "functionalities/recettes_page.html", context)
 
+
 def get_data(request):
     # Récupération du titre du film envoyé depuis la requête GET
-    film_title = request.GET.get('film')
+    film_id = request.GET.get("film")
+    salle = request.GET.get("salle")
 
     # Filtrage des films par titre
-    films = FilmScrap.objects.filter(title=film_title)
+    film = FilmScrap.objects.filter(id=film_id).first()
 
-    # Exemples de limites de capacité pour chaque salle
-    salle_capacite = {"Salle1": 120, "Salle2": 80}
-
-    films_data = []
-
-    for film in films:
-        film_data = {
-            'title': film.title,
-            'id': film.id,
-            'score_pred': film.score_pred,
-            'pred_spect_daily': None,
-            'pred_rct_daily': None,
-            'pred_bnf_weekly': None,
-        }
-
-        if film.score_pred is not None:
-            pred_spect_daily = film.score_pred / 2000 / 7
-            # Utilisation de la capacité de la première salle par défaut
-            film_data['pred_spect_daily'] = min(pred_spect_daily, salle_capacite["Salle1"])
-
-        for salle, capacite in salle_capacite.items():
-            # Calcul de pred_rct_daily avec une limite de capacité
-            film_data['pred_rct_daily_' + salle] = min(film_data['pred_spect_daily'] * 10, capacite) if film_data['pred_spect_daily'] is not None else None
-            film_data['pred_rct_weekly_' + salle] = film_data['pred_rct_daily_' + salle] * 7 if film_data['pred_rct_daily_' + salle] is not None else None
-            film_data['pred_bnf_hebdo_' + salle] = (-4900 + film_data['pred_rct_weekly_' + salle]) if film_data['pred_rct_weekly_' + salle] is not None else None
-
-        films_data.append(film_data)
-
-    response_data = {
-        "films": films_data
+    film_data = {
+        "title": film.title,
+        "id": film.id,
+        "score_pred": film.score_pred,
+        "pred_spect_daily": None,
+        "pred_rct_daily": None,
+        "pred_bnf_hebdo": None,
     }
-    return JsonResponse(response_data)
 
+    if film.score_pred is not None:
+        pred_spect_daily = film.score_pred / 2000 / 7
+        # Utilisation de la capacité de la première salle par défaut
+        film_data["pred_spect_daily"] = min(pred_spect_daily, SALLE_CAPACITE["Salle1"])
 
+    capacite = SALLE_CAPACITE[f"Salle{salle}"]
+
+    # Calcul de pred_rct_daily avec une limite de capacité
+    film_data["pred_rct_daily"] = (
+        min(film_data["pred_spect_daily"] * 10, capacite)
+        if film_data["pred_spect_daily"] is not None
+        else None
+    )
+    film_data["pred_rct_weekly"] = (
+        film_data["pred_rct_daily"] * 7
+        if film_data["pred_rct_daily"] is not None
+        else None
+    )
+    film_data["pred_bnf_hebdo"] = (
+        (-4900 + film_data["pred_rct_weekly"])
+        if film_data["pred_rct_weekly"] is not None
+        else None
+    )
+
+    return JsonResponse({"film": film_data})
 
 
 @login_required
@@ -101,13 +112,13 @@ def predict_page(request):
     one_week_later = today + timedelta(days=7)
     next_day = today + timedelta(days=1)
 
-    films = FilmScrap.objects.filter(date__range=(next_day, one_week_later)).order_by("-score_pred")
-
+    films = FilmScrap.objects.filter(date__range=(next_day, one_week_later)).order_by(
+        "-score_pred"
+    )
 
     ranking = 1
 
     for film in films:
-
         if film.score_pred is not None:
             film.classement = ranking
             ranking += 1
@@ -125,7 +136,9 @@ def predict_page(request):
             film.score_pred_divided_divided = None
 
         if film.score_pred_divided_divided is not None:
-            film.recette_journaliere = round(min(film.score_pred_divided_divided * 10, 2000), 2)
+            film.recette_journaliere = round(
+                min(film.score_pred_divided_divided * 10, 2000), 2
+            )
         else:
             film.recette_journaliere = None
 
@@ -133,18 +146,18 @@ def predict_page(request):
             film.recette_hebdomadaire = round(film.recette_journaliere * 7, 2)
         else:
             film.recette_hebdomadaire = None
-        
+
         if film.recette_hebdomadaire is not None:
             film.benefice_hebdomadaire = round(film.recette_hebdomadaire - 4900, 2)
         else:
             film.benefice_hebdomadaire = None
-    
+
     return render(request, "functionalities/prediction_page.html", {"films": films})
 
 
 @login_required
 def historique_page(request):
-    return render(request, 'functionalities/historique_page.html')
+    return render(request, "functionalities/historique_page.html")
 
 
 @login_required
@@ -153,8 +166,10 @@ def nouveautes_page(request):
     one_week_later = today + timedelta(days=7)
     next_day = today + timedelta(days=1)
 
-    films = FilmScrap.objects.filter(date__range=(next_day, one_week_later)).order_by("-score_pred")[:10]
-    fmt = '{0.hours}h {0.minutes}'
+    films = FilmScrap.objects.filter(date__range=(next_day, one_week_later)).order_by(
+        "-score_pred"
+    )[:10]
+    fmt = "{0.hours}h {0.minutes}"
 
     ranking = 1
 
@@ -168,14 +183,14 @@ def nouveautes_page(request):
         if film.duration is not None:
             film.duration = fmt.format(rd(seconds=film.duration))
         if isinstance(film.genre, list):
-            film.genre = ', '.join(film.genre)
+            film.genre = ", ".join(film.genre)
         if isinstance(film.casting, list):
-            film.casting = ', '.join(film.casting)
-            film.casting = capitalize_name(film.casting)  
+            film.casting = ", ".join(film.casting)
+            film.casting = capitalize_name(film.casting)
         if isinstance(film.director, list):
-            film.director = ', '.join(film.director)
-            film.director = capitalize_name(film.director)  
+            film.director = ", ".join(film.director)
+            film.director = capitalize_name(film.director)
         else:
-            film.director = capitalize_name(film.director)  
-    
+            film.director = capitalize_name(film.director)
+
     return render(request, "functionalities/nouveautes_page.html", {"films": films})
--- Django/static/js/recettes.js
+++ Django/static/js/recettes.js
@@ -1,31 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Fonction pour mettre à jour les données des films sélectionnés
     function updateFilmData(filmData) {
         // Boucle sur les données des films
-        $.each(filmData, function(index, film) {
-            // Mise à jour des éléments HTML avec les données des films
-            $('#pred_rct_daily_' + (index + 1)).text(film.pred_rct_daily);
-            $('#pred_bnf_hebdo_' + (index + 1)).text(film.pred_bnf_hebdo);
-    
-            // Ajout d'un console.log() pour vérifier les données
-            console.log("Film " + (index + 1) + " : Rct = " + film.pred_rct_daily + ", Bnf = " + film.pred_bnf_hebdo);
+        $.each(filmData, function (index, film) {
+            updateOneFilm(film, index + 1)
         });
     }
 
+    function updateOneFilm(film, index = 0) {
+        // Mise à jour des éléments HTML avec les données des films
+        $('#pred_rct_daily_' + index).text(film.pred_rct_daily);
+        $('#pred_bnf_hebdo_' + index).text(film.pred_bnf_hebdo);
+
+        // Ajout d'un console.log() pour vérifier les données
+        console.log("Film " + index + " : Rct = " + film.pred_rct_daily + ", Bnf = " + film.pred_bnf_hebdo);
+    }
+
     // Écouteur d'événements pour le changement de sélection de film
-    $('.film-select').change(function() {
+    $('.film-select').change(function () {
         var filmId = $(this).val(); // Récupérer l'ID du film sélectionné
+        var index = $(this).attr('data-index') * 1
 
         // Requête AJAX pour obtenir les données du film sélectionné
         $.ajax({
             url: '/funct/get_data/', // Utilisation de l'URL correcte
             method: 'GET',
-            data: { film: filmId },
-            success: function(response) {
-                var filmData = response['response-data'];
-                updateFilmData(filmData);
+            data: { film: filmId, salle: index },
+            success: function (response) {
+                updateOneFilm(response.film, index);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error('Erreur lors de la récupération des données du film:', error);
             }
         });
